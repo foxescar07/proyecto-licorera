fed54e18351833ebd96f5f325d0ea8c5e4f76a75
foxescar <ccanariasogamoso@gmail.com>
2026-05-27 12:01:42 -0500

    feat: se realiza el acceso de el html se usuarios

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    # Página usuario
+    path('usuario/', views.usuario, name='usuario'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,18 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 def login_view(request):
-    from django.contrib.auth.forms import AuthenticationForm
     form = AuthenticationForm(request, data=request.POST or None)
+
     if request.method == 'POST':
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('dashboard')  # cámbialo por tu página principal
+            return redirect('dashboard')
+
     return render(request, 'usuarios/login.html', {'form': form})
+
 
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+
+# Vista de usuario
+def usuario(request) -> HttpResponse:
+    return render(request, 'usuario.html')
