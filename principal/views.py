c8204cbc28ba00784b23c2b4f81e6c61c2642652
foxescar <ccanariasogamoso@gmail.com>
2026-05-28 10:30:28 -0500

    feat: modificaciones de enlaces principalmente

--- principal/views.py
+++ principal/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def principal(request):
+    if not request.session.get('usuario_id'):
+        return redirect('login')
     return render(request, 'principal.html')
 
+def cerrar_sesion(request):
+    request.session.flush()
+    return redirect('login')
