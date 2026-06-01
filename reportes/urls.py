3d95c7effab917bb77ba6ada91cb8a99fb673896
ayalamafe35-prog <ayalamafe35@gmail.com>
2026-06-01 10:14:49 -0500

    Feat: se agregaron cosas

--- reportes/urls.py
+++ reportes/urls.py
@@ -5,7 +5,7 @@ app_name = 'reportes'
 
 urlpatterns = [
     # Mapea http://127.0.0.1:8000/reportes/
-    path('', views.index_reportes, name='index'),
+    path('', views.index_reportes, name='reportes'),
     
     # Mapea http://127.0.0.1:8000/reportes/movimientos/
     path('movimientos/', views.reporte_movimientos, name='reporte_movimientos'),
