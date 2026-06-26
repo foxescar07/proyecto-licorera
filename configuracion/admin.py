499cd5bdab9243484b47236e14e528faaa71bc9c
foxescar <ccanariasogamoso@gmail.com>
2026-06-26 09:18:00 -0500

    feat: cambio y establecimiento de confuguracion

--- configuracion/admin.py
+++ configuracion/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ConfiguracionEmpresa, BackupRegistro
 
-# Register your models here.
+@admin.register(ConfiguracionEmpresa)
+class ConfiguracionEmpresaAdmin(admin.ModelAdmin):
+    list_display = ('nombre_empresa', 'nit', 'moneda', 'iva_porcentaje')
+
+@admin.register(BackupRegistro)
+class BackupRegistroAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'fecha', 'tamaño_mb')
+    readonly_fields = ('fecha',)
