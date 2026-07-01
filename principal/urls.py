a341fa0a5da8dcfcd2f31258309232a85fe785ae
foxescar <ccanariasogamoso@gmail.com>
2026-07-01 17:54:11 -0500

    feat: cambios en configuracion

--- principal/urls.py
+++ principal/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.principal, name='principal'),
     path('cerrar-sesion/', views.cerrar_sesion, name='cerrar_sesion'),
+    path('semana-json/', views.semana_json, name='semana_json'),
+    path('meses-json/', views.meses_json, name='meses_json'),
 ]
