16bc8f2d1dafb2f2fb5751a5550530f4ea0670a9
vanessaza06 <yurivanesaaza@gmail.com>
2026-08-10 08:05:54 -0500

    Configurar MySQL como base de datos compartida

--- core/settings.py
+++ core/settings.py
@@ -108,8 +108,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyecto_licorera',
+        'USER': 'root',
+        'PASSWORD': 'vanesaaza2005*',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
