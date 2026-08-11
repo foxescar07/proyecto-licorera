2aa8d7bfefb92c44e030b87b31530f6719faa7da
vanessaza06 <yurivanesaaza@gmail.com>
2026-08-11 10:59:36 -0500

    fix:arreglo

--- core/settings.py
+++ core/settings.py
@@ -108,12 +108,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='proyecto_licorera'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
