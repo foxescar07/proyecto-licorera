4ea262dc363a41c23266ad157e6acc12b91359c7
vanessaza06 <yurivanesaaza@gmail.com>
2026-08-26 15:08:50 -0500

    arreglos models base

--- core/urls.py
+++ core/urls.py
@@ -2,38 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from principal import views as principal_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Página principal
-    path('', principal_views.principal, name='principal'),
-
-    # App usuarios
-    path('usuarios/', include('usuarios.urls')),
-
-    # App productos e inventario
-    path('productos/', include('productos.urls')),
-    path('detalle-producto/', include('detalle_producto.urls', namespace='detalle_producto')),
-    path('categorias/', include('categorias.urls', namespace='categorias')),
-    path('inventario/', include('inventario.urls')),
-    path('lotes/', include('lotes.urls', namespace='lotes')),
-
-    # App reportes
-    path('reportes/', include('reportes.urls')),
-
-    # App ventas
-    path('ventas/', include('ventas.urls')),
-
-    # App proveedores
-    path('proveedores/', include('proveedores.urls')),
-
-    # App configuracion
-    path('configuracion/', include('configuracion.urls')),
-    
-    path('ayuda/', include('ayuda.urls')),
-
-    path('bodega/', include('bodega.urls')),
-
+    path('', include('base.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
